70d4d3e2421fa2a13931f39e8837cdf340f10586
Rich Choy <rich.choy@gmail.com>
2025-05-22 10:31:02 -0700

    0.0.9

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { PureReducerContext, MinimalWorldStateProjection } from '~/types/domain';
 export { EventType } from '~/types/event';
+export { CommandType } from '~/types/intent';
 import { MoveCommandHandler } from '~/command/MOVE/handler';
 
 /**
