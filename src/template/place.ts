ea84fadc29c78fe05e73f2ede5a75707898d4ae1
Rich Choy <rich.choy@gmail.com>
2025-06-30 09:24:03 -0700

    0.2.7

--- src/template/place.ts
+++ src/template/place.ts
@@ -1,8 +1,16 @@
+import { EmergentNarrative } from '~/types';
 import { Template } from "~/types/template";
 import { PlaceSummaryLike } from "~/worldkit/view/place";
 
 export type PlaceTemplateProps = { place: PlaceSummaryLike };
 export type PlaceTemplate = Template<PlaceTemplateProps>;
+
+export const normalizePlaceDescription = (description: string | EmergentNarrative) => {
+  if (typeof description === 'string') {
+    return { base: description, emergent: '' };
+  }
+  return description;
+};
 
 export const renderExits: PlaceTemplate = ({ place }) => {
   return Object.entries(place.exits).map(([direction, exit]) => {
@@ -11,17 +19,12 @@ export const renderExits: PlaceTemplate = ({ place }) => {
 };
 
 export const renderPlaceDescription: PlaceTemplate = ({ place }) => {
-  if (typeof place.description === 'string') {
-    return place.description;
-  }
-
-  const { base, emergent } = place.description;
-  return `${base}\n${emergent}`;
+  const { base, emergent } = normalizePlaceDescription(place.description);
+  const exits = renderExits({ place });
+  return `${base}\n${emergent}\n${exits}`;
 };
 
-export const renderPlaceSummary: PlaceTemplate = (props) => {
-  const description = renderPlaceDescription(props);
-  const exits = renderExits(props);
-
-  return `${props.place.name}\n${description}\n\n${exits}`;
-};
+/**
+ * @deprecated Use `renderPlaceDescription` instead
+ */
+export const renderPlaceSummary = renderPlaceDescription;
