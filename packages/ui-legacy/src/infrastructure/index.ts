b3ee1df80da7b1790965788aaa33cc65a447d5c3
Rich Choy <rich.choy@gmail.com>
2025-10-09 11:37:58 -0700

    Delete Vue3 ui package

--- packages/ui-legacy/src/infrastructure/index.ts
+++
@@ -1,6 +0,0 @@
-// Infrastructure exports for @flux/ui
-export * from './theme';
-export * from './storage';
-export * from './logging/composables';
-export * from './environment/composables';
-export * from './timing/composables';
--- packages/ui-legacy/src/infrastructure/theme/index.ts
+++
@@ -1,3 +0,0 @@
-export { useTheme, type ThemeDependencies, DEFAULT_THEME_DEPENDENCIES } from './composables';
-export { type ThemeColors, type ThemeConfig, type ThemeName } from './types';
-export { darkTheme } from './themes/dark';
